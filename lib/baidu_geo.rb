77afce36f5e2043aa0569f7076aa25f7d7b1630a
luolin <luolinae86@gmail.com>
2017-10-25 22:06:52 +0800

    add address_to_geo and geo_to_address functions

--- lib/baidu_geo.rb
+++ lib/baidu_geo.rb
@@ -3,17 +3,20 @@ require 'rest-client'
 require 'json'
 
 module BaiduGeo
-  def self.address_to_geo(address,ak)
+  AK = "vSpq0zwhjlzDsK8IG5ox7nZNpvs1chjd"
+
+  def self.address_to_geo(address,ak=AK)
     url = URI.encode("http://api.map.baidu.com/geocoder/v2/?address=#{address}&output=json&ak=#{ak}")
-    result = RestClient.get(url)
-    if 200 == result.code
-      body = JSON.parse(result.body)
-      #=> {"status"=>0, "result"=>{"location"=>{"lng"=>103.90207928818799, "lat"=>30.803134859560025}, "precise"=>0, "confidence"=>60, "level"=>"地产小区"}}
-    end
+    result = JSON.parse(RestClient.get(url).body,symbolize_names: true)[:result]
+    {lng: result[:location][:lng],lat: result[:location][:lat],result: result}
   end
 
 
-  def self.geo_to_address(lat,lng,ak)
+  def self.geo_to_address(lat,lng,ak=AK)
+    url = URI.encode("http://api.map.baidu.com/geocoder/v2/?location=#{lat},#{lng}&output=json&pois=1&ak=#{ak}")
+    result = JSON.parse(RestClient.get(url).body, symbolize_names: true)[:result]
+    addr= result[:addressComponent]
+    {province: addr[:province],city: addr[:city],district: addr[:district],street: addr[:street],result: result}
   end
 
 end
